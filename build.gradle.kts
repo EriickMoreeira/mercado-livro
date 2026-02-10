ab1c50c7a479e81e43e5ca4f7dc291b61a46d461
Erick <eriickmoreeira@gmail.com>
2026-02-10 07:52:17 -0300

    Adicionando Beans e Configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,13 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
+	implementation("io.springfox:springfox-boot-starter:3.0.0")
+
+
+
+
+
+
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
 @RestController
-@RequestMapping("customer")
+@RequestMapping("/customer")
 class CustomerController(val customerService: CustomerService) {
 
     @GetMapping
